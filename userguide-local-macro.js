4a93936466afaccb2641348d41bb03daeaf32688
William Mills <wmpmills@gmail.com>
2023-04-04 19:40:49 +0100

    Removed UI Extension XML version number

--- userguide-local-macro.js
+++ userguide-local-macro.js
@@ -187,7 +187,6 @@ function createPanel() {
   }
   const panel = `
     <Extensions>
-    <Version>1.9</Version>
     <Panel>
       <Location>${config.showInCall ? 'HomeScreenAndCallControls' : 'HomeScreen'}</Location>
       <Icon>Help</Icon>
